d2193bf6ec570de6567c9e29c7a5b9089a083b8c
JWolf1306 <jwolf8270@floridapoly.edu>
2025-01-16 16:27:01 +0000

    Implemented variables_intro and student_info

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,20 @@
+// one-line comment - text the compiler ignores
+// This is the first program which prints a welcoming message.
+
+/*
+
+Multiline comment.
+
+January 9th 2025.
+
+ */
+
+#include <stdio.h>
+
+// Create a main function.
+int main() {
+    printf("Welcome\nto\nCOP2271!\n");
+    printf("I\nwant\nto\nsleep.\n");
+
+    return 0;
+}
---
+++ student_info/student_info.c
@@ -0,0 +1,17 @@
+/*
+    Student Info
+    Introductory Program Practice
+*/
+
+#include <stdio.h>
+
+int main( ){
+    int courseID = 2271;
+    int studentID = 18270;
+    printf("Name: Jeffrey Wolf\n");
+    printf("Course ID: %d\n", courseID);
+    printf("Student ID: %d\n", studentID);
+    printf("Student ID - Course ID is: %d\n", studentID - courseID);
+    
+    return 0;
+}
